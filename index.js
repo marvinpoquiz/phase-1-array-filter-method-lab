f77847a8c661f2c05dad93c9a13c31f1b50db625
Marvin <marvinepoquiz@gmail.com>
2021-11-11 07:08:40 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, item) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === item.toLowerCase() 
+    })
+  }
+  
+  function fuzzyMatch(drivers, item) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase().substring(0, item.length) === item.toLowerCase() 
+    })
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(function (driver) { 
+        return driver.name === name })
+  }
+  
